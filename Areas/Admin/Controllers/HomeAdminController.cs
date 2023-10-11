85ae18d192710fb9cb3122969c8c3a7e6ce02172
bino <hoangpham23052003@gmail.com>
2023-10-12 00:55:08 +0700

    Demo Layout  Admin

---
+++ Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTL_QuanLyBanDienThoai.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin")]
+    public class HomeAdminController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
