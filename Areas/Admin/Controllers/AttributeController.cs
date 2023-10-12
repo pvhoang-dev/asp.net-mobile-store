0c457f824a45e426412e64c7373f504e3ee63f65
bino <hoangpham23052003@gmail.com>
2023-10-12 23:13:22 +0700

    Attr display

---
+++ Areas/Admin/Controllers/AttributeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTL_QuanLyBanDienThoai.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin/attributes")]
+    public class AttributeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [Route("edit")]
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
