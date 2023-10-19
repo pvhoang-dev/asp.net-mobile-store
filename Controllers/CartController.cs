f7345466c04e3686675591946b7153e63e8bb271
Nhiem130303 <nhiemnd1303@gmail.com>
2023-10-20 00:59:55 +0700

    demo si đần

---
+++ Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTL_QuanLyBanDienThoai.Controllers
+{
+	public class CartController : Controller
+	{
+		[Route("home/carts")]
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
