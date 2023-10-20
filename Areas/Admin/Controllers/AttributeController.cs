9834c654e87dcae4037683cfe437ea2b9d8df840
Nguyen Duy Khanh <nguyenkhanh17112003@gmail.com>
2023-10-20 09:40:25 +0700

    feat: attribute controler view

--- Areas/Admin/Controllers/AttributeController.cs
+++ Areas/Admin/Controllers/AttributeController.cs
@@ -1,26 +1,107 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BTL_QuanLyBanDienThoai.Data;
+using BTL_QuanLyBanDienThoai.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BTL_QuanLyBanDienThoai.Areas.Admin.Controllers
 {
+
+
     [Area("admin")]
     [Route("admin/attributes")]
     public class AttributeController : Controller
     {
-        public IActionResult Index()
+        private readonly QLBanDienThoaiContext db;
+
+        public AttributeController(QLBanDienThoaiContext _db)
         {
-            return View();
+            this.db = _db;
         }
 
-        [Route("create")]
+        public IActionResult Index()
+        {
+            List<Attr> attrs = db.Attrs.ToList();
+            return View(attrs);
+            // return View();
+        }
+
+        [Route("Create")]
         public IActionResult Create()
         {
             return View();
         }
 
-        [Route("edit")]
-        public IActionResult Edit()
+        [Route("Create")]
+        [HttpPost]
+        public IActionResult Create(Attr attr)
         {
+            if (ModelState.IsValid)
+            {
+                this.db.Attrs.Add(attr);
+                this.db.SaveChanges();
+                return RedirectToAction("Index", "Attribute");
+            }
             return View();
         }
+
+        [Route("Edit/{id}")]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Attr attr = db.Attrs.Find(id);
+            if (attr == null)
+            {
+                return NotFound();
+            }
+            return View(attr);
+        }
+
+        [Route("Edit/{id}")]
+        [HttpPost]
+        public IActionResult Edit(Attr attr)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Attrs.Update(attr);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Attribute");
+
+            }
+            return View();
+        }
+
+        [Route("Delete/{id}")]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Attr attr = this.db.Attrs.Find(id);
+            if (attr == null)
+            {
+                return NotFound();
+            }
+            return View(attr);
+        }
+
+        [Route("Delete/{id}")]
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Attr? attr = db.Attrs.Find(id);
+            if (attr == null)
+            {
+                return NotFound();
+            }
+
+            db.Attrs.Remove(attr);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Attribute");
+        }
+
+        
     }
 }
---
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,91 @@
+﻿using BTL_QuanLyBanDienThoai.Data;
+using BTL_QuanLyBanDienThoai.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BTL_QuanLyBanDienThoai.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly QLBanDienThoaiContext db;
+
+        public CategoryController(QLBanDienThoaiContext _db)
+        {
+            db = _db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = db.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0) 
+            {
+                return NotFound();
+            }
+            Category category = db.Categories.Find(id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Categories.Update(category);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Category");
+        }
+    }
+}
