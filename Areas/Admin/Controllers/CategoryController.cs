4142034ed15153828dcd9c07f509d5d250357499
bino <hoangpham23052003@gmail.com>
2023-10-23 01:56:18 +0700

    Done Category

--- Areas/Admin/Controllers/CategoryController.cs
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -1,7 +1,10 @@
-using BTL_QuanLyBanDienThoai.Data;
+﻿using BTL_QuanLyBanDienThoai.Data;
 using BTL_QuanLyBanDienThoai.Models;
 using BTL_QuanLyBanDienThoai.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting.Internal;
+using Newtonsoft.Json;
 using System.Diagnostics;
 using System.Web;
 
@@ -13,11 +16,16 @@ namespace BTL_QuanLyBanDienThoai.Areas.Admin.Controllers
     {
         private readonly QLBanDienThoaiContext db;
         readonly IBufferedFileUploadService _bufferedFileUploadService;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public CategoryController(QLBanDienThoaiContext _db, IBufferedFileUploadService bufferedFileUploadService)
+        public CategoryController(
+            QLBanDienThoaiContext _db,
+            IBufferedFileUploadService bufferedFileUploadService,
+            IWebHostEnvironment webHostEnvironment)
         {
             db = _db;
             _bufferedFileUploadService = bufferedFileUploadService;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -35,7 +43,7 @@ namespace BTL_QuanLyBanDienThoai.Areas.Admin.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(Category category, IFormFile cateImg)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 if (cateImg != null)
                     try
@@ -57,9 +65,9 @@ namespace BTL_QuanLyBanDienThoai.Areas.Admin.Controllers
                         ViewBag.Message = "File Upload Failed";
                     }
 
-                if (category.Image != null)              
+                if (category.Image != null)
                     category.Image = Path.Combine("UploadedFiles\\categories", category.Image);
-                    
+
                 db.Categories.Add(category);
                 db.SaveChanges();
                 return RedirectToAction("Index", "Category");
@@ -69,22 +77,112 @@ namespace BTL_QuanLyBanDienThoai.Areas.Admin.Controllers
         }
 
         [Route("Edit/{id}")]
-        public IActionResult Edit()
+        public IActionResult Edit(int? id)
         {
-            return View();
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category cate = db.Categories.Find(id);
+            if (cate == null)
+            {
+                return NotFound();
+            }
+            return View(cate);
         }
 
         [Route("Edit/{id}")]
         [HttpPost]
-        public IActionResult Edit(Category category)
+        public async Task<IActionResult> Edit(Category category, IFormFile cateImg, int? id)
         {
-            if(ModelState.IsValid)
+            Category cate = db.Categories.Find(category.Id);
+
+            if (ModelState.IsValid)
             {
-                db.Categories.Update(category);
-                db.SaveChanges();
-                return RedirectToAction("Index", "Category");
+                if (cateImg != null)
+                    try
+                    {
+                        string filePath = Path.Combine(_webHostEnvironment.WebRootPath, cate.Image);
+
+                        if (System.IO.File.Exists(filePath))
+                        {
+                            System.IO.File.Delete(filePath);
+                        }
+
+                        if (category.Image != null)
+                            category.Image = Path.Combine("UploadedFiles\\categories", category.Image);
+
+                        cate.Image = category.Image;
+
+                        if (await _bufferedFileUploadService.UploadFile(cateImg, "categories"))
+                        {
+                            ViewBag.Message = "Edit Category Successful";
+                            ViewBag.Text = "success";
+                        }
+                        else
+                        {
+                            ViewBag.Message = "File Upload Failed";
+                            ViewBag.Text = "warning";
+                        }
+                    }
+                    catch
+                    {
+                        ViewBag.Message = "File Upload Failed";
+                        ViewBag.Text = "warning";
+                    }
             }
-            return View();
+
+            ViewBag.Message = "Edit Category Successful";
+            ViewBag.Text = "success";
+
+            cate.Name = category.Name;
+
+            db.Categories.Update(cate);
+            db.SaveChanges();
+
+            return View(cate);
+        }
+
+        [Route("Delete")]
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var dbCat = db.Categories.FirstOrDefault(x => x.Id == id);
+
+            if (dbCat != null)
+            {
+                db.Categories.Remove(dbCat);
+                try
+                {
+                    db.SaveChanges();
+
+                    string filePath = Path.Combine(_webHostEnvironment.WebRootPath, dbCat.Image);
+
+                    if (System.IO.File.Exists(filePath))
+                    {
+                        System.IO.File.Delete(filePath);
+                    }
+
+                    return Json(new { success = true, message = "done" });
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.ToString());
+                    return BadRequest(JsonConvert.SerializeObject(
+                        new
+                        {
+                            error = "Can not delete this attribute."
+                        }
+                    ));
+                }
+            }
+
+            return BadRequest(JsonConvert.SerializeObject(
+                new
+                {
+                    error = "Can not delete this attribute."
+                }
+            ));
         }
     }
 
