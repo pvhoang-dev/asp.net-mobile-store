9f9e89f4d974e89e0be07868f130d8b17ad9c1c8
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2023-10-27 11:22:05 +0700

    update

--- Models/ViewModel/AttributeValueViewModel.cs
+++ Models/ViewModel/AttributeValueViewModel.cs
@@ -1,10 +1,17 @@
-﻿namespace BTL_QuanLyBanDienThoai.Models.ViewModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BTL_QuanLyBanDienThoai.Models.ViewModel
 {
     public class AttributeValueViewModel
     {
         public List<Attr>? attrs { get; set; }
+        
         public int? attrId { get; set; }
         public string? Value { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter the attribute value name.")]
+        [MaxLength(50)]
         public string? Name { get; set; }
         public int? Id { get; set; }
     }
