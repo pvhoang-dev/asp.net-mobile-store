409b2567181771dff374bdbece54305aafab1639
Nhiem130303 <nhiemnd1303@gmail.com>
2023-10-29 15:41:02 +0700

    no message

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BTL_QuanLyBanDienThoai.Models.ViewModel;
 using static Microsoft.Extensions.Logging.EventSource.LoggingEventSource;
+using System.Runtime.CompilerServices;
 
 namespace BTL_QuanLyBanDienThoai.Controllers
 {
@@ -42,6 +43,41 @@ namespace BTL_QuanLyBanDienThoai.Controllers
 			};
 
 			return View(viewModel);
+		}
+
+		[Route("products/ajax-search-price")]
+		[HttpGet]
+		public ActionResult AjaxSearch(double priceMin, double priceMax)
+		{
+			if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+			{
+				if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+				{
+					try
+					{
+						var data = _context.Products
+						.Where(p => p.Price >= priceMin && p.Price <= priceMax)
+						.Select(p => new
+						{
+							p.Id,
+							p.Name,
+							p.ImageDefault,
+							p.Price,
+							p.Price2,
+							p.CategoryId
+						})
+						.ToList();
+
+						return Json(new { success = true, data = data });
+					}
+					catch (Exception ex)
+					{
+						return Json(new { success = false, error = ex.Message });
+					}
+				}
+			}
+
+			return View();
 		}
 	}
 }
